422da60b7a1ee69e5eaa24ed2ad1e97211cb01fb
Dhaniel Aritonang <65146994+aritonang92@users.noreply.github.com>
2020-07-01 13:05:59 +0700

    Give some information to the rows

--- socialnetworkads.R
+++ socialnetworkads.R
@@ -1,4 +1,4 @@
-# Getting data
+# Getting data from Computer
 social_network <- read.csv(file.choose(), header = TRUE)
 
 # Data Preparation
@@ -9,10 +9,10 @@ social_network$Gender <- as.factor(social_network$Gender)  # Makes Gender column
 
 str(social_network)
 
-# Two way table of factor variables
+# Two way table of factor variables to see each distribution
 xtabs(~Gender + Purchased, data = social_network)
 
-# Partition data - train (80%) & test (20%)
+# Partition data - train (80%) & test (20%). Take a random sampling
 set.seed(1234)
 ind <- sample(2, nrow(social_network), replace = TRUE,
               prob = c(0.8, 0.2))
@@ -29,17 +29,18 @@ p1 <- predict(mymodel, train, type = "response")
 head(p1)
 head(train)
 
-# Misclassification error - train data
+# Misclassification error of train data
 predict1 <- ifelse(p1 > 0.5,1,0) # Makes own prediction stated that If p1 > 0.5 then he/she will purchased
-tab1 <- table(Prediction = predict1, Actual = train$Purchased) # This is confusion matrix
-1 - sum(diag(tab1)) / sum(tab1) # Misclassification error
+tab1 <- table(Prediction = predict1, Actual = train$Purchased) # This is confusion matrix formed by train data
+(1 - sum(diag(tab1)) / sum(tab1))*100 # Misclassification error is 16 %
 
-# Misclassification error - test data
+# Misclassification error of test data
 p2 <- predict(mymodel, test, type = "response")
 predict2 <- ifelse(p2 > 0.5,1,0) # Makes own prediction stated that If p1 > 0.5 then he/she will purchased
-tab2 <- table(Prediction = predict2, Actual = test$Purchased) # This is confusion matrix
-1 - sum(diag(tab2)) / sum(tab2) # Misclassification error
+tab2 <- table(Prediction = predict2, Actual = test$Purchased) # This is confusion matrix formed by test data
+(1 - sum(diag(tab2)) / sum(tab2))*100 # Misclassification error is 13.33 %
 
 # Goodness of fit
 with(mymodel, pchisq(null.deviance - deviance,
-                     df.null-df.residual, lower.tail = F)) # Tye result is 1.547195e-41
+                     df.null-df.residual, lower.tail = F)) # The result is 1.547195e-41
+
